3eb9ba059d66498d134da653ba9c1f04bc7a9821
KOSSOU <3673539@ppti-14-503-07.ufr-info-p6.jussieu.fr>
2017-03-29 10:24:20 +0200

    heroku commit 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const userBramas = new Discord.User();
 
-userBramas.username = bramas;
+userBramas.username = 'bramas';
 
 client.on('ready', () => {
 	console.log('I am ready!');

9dbfee947f58f091234aa4f36893b7e075447798
KOSSOU <3673539@ppti-14-503-07.ufr-info-p6.jussieu.fr>
2017-03-29 10:21:02 +0200

    heroku commit 2

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
+const userBramas = new Discord.User();
+
+userBramas = {"username": "bramas"}
+
 client.on('ready', () => {
-console.log('I am ready!');
+	console.log('I am ready!');
+	console.log('presence : '+userBramas.presence);
 });
+
 client.on('message', message => {
-if (message.content === 'ping') {
-message.reply('pong');
-}
-console.log(message);
+	if (message.content === 'ping') {
+		message.reply('pong');
+	}
+	console.log(message);
 });
+
 client.on('presenceUpdate', function(oldMember, newMember) {
-console.log(oldMember.presence, '=>', newMember.presence);
+	console.log(oldMember.presence, '=>', newMember.presence);
 });
+
 client.login(process.env.DISCORD_TOKEN);

178bd01cd50befef7531186c239c0884424e5d9c
KOSSOU <3673539@ppti-14-503-07.ufr-info-p6.jussieu.fr>
2017-03-29 13:58:28 +0200

    heroku commit 2

--- meteo.js
+++ meteo.js
@@ -5,11 +5,12 @@ module.exports = function(msg, callback){
 	if(content[0].trim() == '!meteo'){
 		if(content.length > 2){
 			callback('Message incompris');
+			return;
 		}
 		axios.get('http://api.openweathermap.org/data/2.5/forecast/daily?q='+content[1].trim()+'&mode=json&units=metric&cnt=1&appid=99a63ddd6a41fc62bc4399029cffad4c')
 		.then(function(rep){
 			var forecast = {"name":rep.data.city.name,  "temp": rep.data.list[0].temp.day ," weather" : rep.data.list[0].weather[0].description};
-			callback(forecast);
+			callback(JSON.stringify(forecast));
 		}).catch(console.error);
 	}
 	

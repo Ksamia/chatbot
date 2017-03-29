70df46a7bca292ccffaabe35c8a5ce5393111a03
KOSSOU <3673539@ppti-14-503-07.ufr-info-p6.jussieu.fr>
2017-03-29 10:30:55 +0200

    heroku commit 2

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ client.on('message', message => {
 
 client.on('presenceUpdate', function(oldMember, newMember) {
 	console.log(oldMember.presence, '=>', newMember.presence);
+	console.log('oldmember username :',oldMember.user.username);
+	console.log('newmember username :',newMember.user.username);
 });
 
 client.login(process.env.DISCORD_TOKEN);

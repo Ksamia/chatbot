8defc5eba39b6a7450a42cc3cde9e840fe44a2bf
KOSSOU <3673539@ppti-14-503-12.ufr-info-p6.jussieu.fr>
2017-04-26 11:57:37 +0200

    herocku commit

--- iss.js
+++ iss.js
@@ -5,9 +5,10 @@ var coordinates = {long:0, lat:0, img:''};
 module.exports = function(callback){
 	axios.get('https://api.wheretheiss.at/v1/satellites/25544')
 		.then(function(rep){
-			coordinates.long = rep.longitude;
-			coordinates.lat = rep.latitude;
-			coordinates.link = 'http://staticmap.openstreetmap.de/staticmap.php?center='+rep.latitude+','+rep.longitude+'&zoom=5size=400x300&maptype=mapnik&markers='+rep.latitude+','+rep.longitude+'ltblu-pushpin';
+			console.log(rep);
+			//coordinates.long = rep.longitude;
+			//coordinates.lat = rep.latitude;
+			//coordinates.img = 'http://staticmap.openstreetmap.de/staticmap.php?center='+rep.latitude+','+rep.longitude+'&zoom=5size=400x300&maptype=mapnik&markers='+rep.latitude+','+rep.longitude+'ltblu-pushpin';
 			callback(JSON.stringify(coordinates));
 		})
 		.catch(console.error);

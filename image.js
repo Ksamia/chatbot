186be711871fd73ceabc103359d4da43e71c6c14
KOSSOU <3673539@ppti-14-503-12.ufr-info-p6.jussieu.fr>
2017-04-26 11:15:50 +0200

    herocku commit

--- image.js
+++ image.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 module.exports = function(searchString,callback){
-	axios.get("https://api.imgur.com/3/gallery/search/q?=" + searchString, {headers : {'Authorization': "Client-ID e3ca79d47a9f4d5"}})
+	axios.get("https://api.imgur.com/3/gallery/search/?q=" + searchString, {headers : {'Authorization': "Client-ID e3ca79d47a9f4d5"}})
 		.then(function(rep){
 			//console.log(rep);
 			//var forecast = "name : "+rep.data.city.name+", temperature : "+rep.data.list[0].temp.day+", weather : "+rep.data.list[0].weather[0].description;

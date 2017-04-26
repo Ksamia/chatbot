6fbcdcbff8cf16afbe03ce6473562a755e3cb640
KOSSOU <3673539@ppti-14-503-12.ufr-info-p6.jussieu.fr>
2017-04-26 14:12:20 +0200

    herocku commit

--- iss.js
+++ iss.js
@@ -13,9 +13,6 @@ module.exports = function(callback){
 			coordinates.long = rep.data.longitude;
 			coordinates.lat = rep.data.latitude;
 			coordinates.img = 'http://staticmap.openstreetmap.de/staticmap.php?center='+rep.data.latitude+','+rep.data.longitude+'&zoom=5size=400x300&maptype=mapnik&markers='+rep.data.latitude+','+rep.data.longitude+'ltblu-pushpin';
-			sharp(coordinates.img)
-				.overlayWith('https://i.downloadatoz.com/download/icon2/c/9/5/e3c8de70b0e1170c327696c64c8bc59c.jpg', { top:coordinates.latitude, left:coordinates.longitude } )
-			  	.toFile('output.jpg');
 			callback(JSON.stringify(coordinates));
 		})
 		.catch(console.error);

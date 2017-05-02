d74ae4663cc6e58a2876a22c3106023d966751c9
KOSSOU <3673539@ppti-14-503-10.ufr-info-p6.jussieu.fr>
2017-05-02 19:47:34 +0200

    commit 02 mai

--- iss.js
+++ iss.js
@@ -6,15 +6,8 @@ const fs = require('fs');
 
 var coordinates = {long:0, lat:0, img:''};
 
-module.exports = function(callback){
-	axios.get('https://api.wheretheiss.at/v1/satellites/25544')
-		.then(function(rep){
-			console.log(rep.data.latitude);
-			coordinates.long = rep.data.longitude;
-			coordinates.lat = rep.data.latitude;
-			coordinates.img = 'http://staticmap.openstreetmap.de/staticmap.php?center='+rep.data.latitude+','+rep.data.longitude+'&zoom=5size=400x300&maptype=mapnik&markers='+rep.data.latitude+','+rep.data.longitude+'ltblu-pushpin';
-			//callback(JSON.stringify(coordinates));
-			axios.get(coordinates.img,{ responseType:"arraybuffer" })
+function composeImg(){
+	axios.get(coordinates.img,{ responseType:"arraybuffer" })
 			.then(function(rep){
 				//console.log(rep.data)
 				sharp(rep.data)
@@ -23,11 +16,24 @@ module.exports = function(callback){
 					.toBuffer(function(error,data,info){
 						console.log('toBuffer data')
 						console.log(info)
-						callback(data.toString('base64'))
+						///callback(data.toString('base64'))
+						return(data)
 					})
-					.catch(console.error);
-			
+					.catch(console.error)
 			})
+}
+
+module.exports = function(callback){
+	axios.get('https://api.wheretheiss.at/v1/satellites/25544')
+		.then(function(rep){
+			console.log(rep.data.latitude);
+			coordinates.long = rep.data.longitude;
+			coordinates.lat = rep.data.latitude;
+			coordinates.img = 'http://staticmap.openstreetmap.de/staticmap.php?center='+rep.data.latitude+','+rep.data.longitude+'&zoom=5size=400x300&maptype=mapnik&markers='+rep.data.latitude+','+rep.data.longitude+'ltblu-pushpin';
+			//callback(JSON.stringify(coordinates));
+			var composition = composeImg();
+			callback(composition);
+			
 		})
 		.catch(console.error);
 }

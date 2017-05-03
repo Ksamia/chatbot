8993689a31f107fcb31b071de398d499f9244cd7
KOSSOU <3673539@ppti-14-501-04.ufr-info-p6.jussieu.fr>
2017-05-03 19:36:17 +0200

    commit 03 mai*

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ client.on('message', message => {
 			getIss.getImgLink(function(imagebuff){
 				console.log(imagebuff);
 				console.log(message);
-				message.author.sendFile(message.channel, './satelite.jpg');
+				message.author.sendFile(message.channel.id, './satelite.jpg');
 			});
 			//console.log('display compose return value');
 			//getIss.Compose(console.log);

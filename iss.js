f7600afc9ccffbecad92b6e7c3bccefa884a7cec
KOSSOU <3673539@ppti-14-503-10.ufr-info-p6.jussieu.fr>
2017-05-03 12:06:05 +0200

    commit 03 mai

--- iss.js
+++ iss.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 
 var coordinates = {long:0, lat:0, img:''};
 
-function getISS(){
+/*function getISS(){
 	console.log('iss.js getISS function');
 	axios.get('https://api.wheretheiss.at/v1/satellites/25544')
 		.then(function(rep){
@@ -16,39 +16,38 @@ function getISS(){
 			coordinates.img = 'http://staticmap.openstreetmap.de/staticmap.php?center='+rep.data.latitude+','+rep.data.longitude+'&zoom=5size=400x300&maptype=mapnik&markers='+rep.data.latitude+','+rep.data.longitude+'ltblu-pushpin';
 		})
 		.catch(console.error);
-}
+}*/
 
-module.exports = function(callback){
-	getISS();
-	console.log('iss.js module.export before axios.get');
+function Compose(){
+	console.log('iss.js compose function');
 	console.log(coordinates.img)
 	axios.get(coordinates.img,{ responseType:"arraybuffer" })
 		.then(function(rep){
-			console.log('iss.js in axios.get');
+			console.log('iss.js in compose axios.get');
 			sharp(rep.data)
 				.overlayWith('./satelite.jpg')
 				.png()
 				.toBuffer(function(error,data,info){
 					console.log('toBuffer data');
 					console.log(info);
-					callback(data);
+					return(data);
 				})
 				.catch(console.error)
 		})
 }
 
-/*module.exports = function(callback){
+module.exports = function(callback){
 	axios.get('https://api.wheretheiss.at/v1/satellites/25544')
 		.then(function(rep){
 			console.log(rep.data.latitude);
 			coordinates.long = rep.data.longitude;
 			coordinates.lat = rep.data.latitude;
 			coordinates.img = 'http://staticmap.openstreetmap.de/staticmap.php?center='+rep.data.latitude+','+rep.data.longitude+'&zoom=5size=400x300&maptype=mapnik&markers='+rep.data.latitude+','+rep.data.longitude+'ltblu-pushpin';
-			//callback(JSON.stringify(coordinates));
-			var composition = composeImg();
-			callback(composition);
-			
+			//callback(JSON.stringify(coordinates))
 		})
 		.catch(console.error);
-}*/
+
+	var composeImg = Compose();
+	callback(composeImg);
+}
 

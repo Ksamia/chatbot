ed87b9da943d8bdcae2e17052c71521cdba1fe7b
KOSSOU <3673539@ppti-14-503-10.ufr-info-p6.jussieu.fr>
2017-05-03 12:08:59 +0200

    commit 03 mai

--- iss.js
+++ iss.js
@@ -6,18 +6,6 @@ const fs = require('fs');
 
 var coordinates = {long:0, lat:0, img:''};
 
-/*function getISS(){
-	console.log('iss.js getISS function');
-	axios.get('https://api.wheretheiss.at/v1/satellites/25544')
-		.then(function(rep){
-			console.log(rep.data.latitude);
-			coordinates.long = rep.data.longitude;
-			coordinates.lat = rep.data.latitude;
-			coordinates.img = 'http://staticmap.openstreetmap.de/staticmap.php?center='+rep.data.latitude+','+rep.data.longitude+'&zoom=5size=400x300&maptype=mapnik&markers='+rep.data.latitude+','+rep.data.longitude+'ltblu-pushpin';
-		})
-		.catch(console.error);
-}*/
-
 function Compose(){
 	console.log('iss.js compose function');
 	console.log(coordinates.img)
@@ -43,11 +31,12 @@ module.exports = function(callback){
 			coordinates.long = rep.data.longitude;
 			coordinates.lat = rep.data.latitude;
 			coordinates.img = 'http://staticmap.openstreetmap.de/staticmap.php?center='+rep.data.latitude+','+rep.data.longitude+'&zoom=5size=400x300&maptype=mapnik&markers='+rep.data.latitude+','+rep.data.longitude+'ltblu-pushpin';
+			var composeImg = Compose();
+			callback(composeImg);
 			//callback(JSON.stringify(coordinates))
 		})
 		.catch(console.error);
 
-	var composeImg = Compose();
-	callback(composeImg);
+	
 }
 

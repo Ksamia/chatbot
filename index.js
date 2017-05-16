aa26c3866212d297dc3ac32ae77b0176f56a3970
KOSSOU <3673539@ppti-14-401-02.ufr-info-p6.jussieu.fr>
2017-05-16 18:58:03 +0200

    commit 16 mai node_modules_cache heroku disabled

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ client.on('message', message => {
 			console.log('index.js if iss condition');
 			getIss(function(imagebuff){
 				console.log(imagebuff)
-				message.author.sendFile(imagebuff.options.input.buffer,"png");
+				message.author.sendFile(imagebuff,"png");
 			});
 		}
 		else{

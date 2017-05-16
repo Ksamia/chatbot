ec6b8199c5f3fd0d997ca3b23695d38e5583b997
KOSSOU <3673539@ppti-14-401-02.ufr-info-p6.jussieu.fr>
2017-05-16 18:09:04 +0200

    commit 16 mai node_modules_cache heroku disabled

--- iss.js
+++ iss.js
@@ -6,46 +6,18 @@ const fs = require('fs');
 
 const coordinates = {long:0, lat:0, img:''};
 
-const overlay = "http://www.clker.com/cliparts/4/a/3/6/13673152381260660812free-global-security-satellite-th.png";
-
-var overlayImg;
-
-axios.get("http://www.clker.com/cliparts/4/a/3/6/13673152381260660812free-global-security-satellite-th.png", {responseType:"arraybuffer"})
-	.then(function(rep){
-		overlayImg = rep.data;
-	}).catch(console.error);
-
-
 module.exports.getImgLink = function getImgLink(callback){
 	axios.get('https://api.wheretheiss.at/v1/satellites/25544')
 		.then(function(rep){
-			//console.log(rep.data.latitude);
 			coordinates.long = rep.data.longitude;
 			coordinates.lat = rep.data.latitude;
 			coordinates.img = 'http://staticmap.openstreetmap.de/staticmap.php?center='+rep.data.latitude+','+rep.data.longitude+'&zoom=5size=400x300&maptype=mapnik&markers='+rep.data.latitude+','+rep.data.longitude+'ltblu-pushpin';
-			
 			axios.get(coordinates.img,{ responseType:"arraybuffer" })
 				.then(function(rep){
-					var buff_image = sharp(rep.data).overlayWith('./iss.png');
+					var buff_image = sharp(rep.data)resize(800).overlayWith('./iss.png');
 					callback(buff_image);
 				}).catch(console.error);
 		})
 		.catch(console.error);
 }
 
-
-
-/*module.exports.Compose = function Compose(callback){
-	console.log('coordinates.img '+coordinates.img);
-	axios.get('http://staticmap.openstreetmap.de/staticmap.php?center=40.465476578251,68.987902965651&zoom=5size=400x300&maptype=mapnik&format=png&markers=40.465476578251,68.987902965651ltblu-pushpin',{ responseType:"arraybuffer" })
-		.then(function(rep){
-			sharp(rep.data)
-				.overlayWith('./satelite.jpg')
-				.png()
-				.toBuffer()
-				.then(function(output){
-					callback(output);
-				})
-				.catch(console.error)
-		}).catch(console.error);
-}*/

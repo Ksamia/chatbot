1875e07095f232807a545ec9f223932782e396a8
KOSSOU <3673539@ppti-14-401-02.ufr-info-p6.jussieu.fr>
2017-05-16 18:10:38 +0200

    commit 16 mai node_modules_cache heroku disabled

--- iss.js
+++ iss.js
@@ -14,7 +14,7 @@ module.exports.getImgLink = function getImgLink(callback){
 			coordinates.img = 'http://staticmap.openstreetmap.de/staticmap.php?center='+rep.data.latitude+','+rep.data.longitude+'&zoom=5size=400x300&maptype=mapnik&markers='+rep.data.latitude+','+rep.data.longitude+'ltblu-pushpin';
 			axios.get(coordinates.img,{ responseType:"arraybuffer" })
 				.then(function(rep){
-					var buff_image = sharp(rep.data)resize(800).overlayWith('./iss.png');
+					var buff_image = sharp(rep.data).resize(100).overlayWith('./iss.png');
 					callback(buff_image);
 				}).catch(console.error);
 		})

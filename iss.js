992d7ae3c353e03abdc3b45bd71e5c1a89a4c854
KOSSOU <3673539@ppti-14-401-02.ufr-info-p6.jussieu.fr>
2017-05-16 18:54:20 +0200

    commit 16 mai node_modules_cache heroku disabled

--- iss.js
+++ iss.js
@@ -6,24 +6,17 @@ var request = require('request').defaults({ encoding: null })
 
 var link;
 var outputImg;
-imgLink = function(){
+module.exports = function(){
 	axios.get('https://api.wheretheiss.at/v1/satellites/25544')
 		.then(function(rep){
 			var link = 'http://staticmap.openstreetmap.de/staticmap.php?center='+rep.data.latitude+','+rep.data.longitude+'&zoom=5size=400x300&maptype=mapnik&markers='+rep.data.latitude+','+rep.data.longitude+'ltblu-pushpin';
-			/*axios.get(link,{ responseType:"arraybuffer" })
+			axios.get(link,{ responseType:"arraybuffer" })
 				.then(function(rep){
-					var buff_image = sharp(rep.data).overlayWith('./iss.png');
-					callback(buff_image);
-				}).catch(console.error);*/
+					sharp(rep.data).overlayWith('./iss.png').toBuffer().then(function(buff){callback(buff);});
+					//callback(buff_image);
+				}).catch(console.error);
 		})
 		.catch(console.error);
 }
 
-module.exports = function(callback){
-	imgLink();
-	request.get(link, function (err, res, body) {
-              sharp(body).overlayWith('./iss.png').toBuffer().then(function(buff){callback(buff)});
-		
-	});
-}
 

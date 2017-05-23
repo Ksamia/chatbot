6120b20a312324277f64888730f0c184fa215bb5
KOSSOU <3673539@ppti-14-503-16.ufr-info-p6.jussieu.fr>
2017-05-23 14:20:57 +0200

    23 mai

--- meteo.js
+++ meteo.js
@@ -1,8 +1,9 @@
 const axios = require('axios');
 
 module.exports = function(city,id, callback){
-	axios.get('http://api.openweathermap.org/data/2.5/forecast/daily?q='+city+'&mode=json&units=metric&cnt=1&appid=99a63ddd6a41fc62bc4399029cffad4c')
+	axios.get('http://api.openweathermap.org/data/2.5/forecast/daily?q='+city+'&mode=json&units=metric&cnt=2&appid=99a63ddd6a41fc62bc4399029cffad4c')
 		.then(function(rep){
+			console.log(rep.data.list);
 			var forecast = "name : "+rep.data.city.name+", temperature : "+rep.data.list[0].temp.day+", weather : "+rep.data.list[0].weather[0].description;
 			callback(forecast);
 		}).catch(console.error);
